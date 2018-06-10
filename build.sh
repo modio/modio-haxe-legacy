bc909c5f4a19df58af8f1e6a57f562206255b3b8
turupawn <ahmed.hn.43@gmail.com>
2018-06-10 14:38:57 -0600

    separate lib directory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -L. ./src/modioGlue.cpp -shared -o ./bin/modioGlue.ndll -I./include -lmodio -fPIC -Wl,-rpath .
+g++ -L./lib/linux64 ./src/modioGlue.cpp -shared -o ./bin/modioGlue.ndll -I./include -lmodio -fPIC -Wl,-rpath .
 haxe -cp src -main Main -cpp bin -D HXCPP_M64
 LD_LIBRARY_PATH=./bin/ ./bin/Main
