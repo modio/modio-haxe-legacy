78831dd566a8d80048be90fc5db9a8aa78d849f1
turupawn <ahmed.hn.43@gmail.com>
2018-06-11 17:13:59 -0600

    windows 32 bits now working

---
+++ VisualBuild32bits.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir bin
+cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp lib/windows32/modio.lib /I include /link /DLL /OUT:bin/modioWrapper.ndll
+haxe -cp src -main Main -cpp bin
+copy lib\windows32\modio.dll bin
+cd bin
+Main.exe
