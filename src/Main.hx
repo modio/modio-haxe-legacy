4971da4674f1e301f0e931a59def48f8ed89cd12
turupawn <ahmed.hn.43@gmail.com>
2018-06-10 21:25:01 -0600

    basic functions implemented

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,66 @@
 class Main 
 {
-    //This loads the test.ndll and 'binds' the Haxe function 'sum' to the C function 'sum'.
-    //Int->Int->Int is Haxe syntax for typing a function that takes 2 integers as arguments and that returns an integer.
-    static var init:Int->Int->String->Void = cpp.Lib.load("modioGlue","init",3);
+    static var modioGlueInit:Int->Int->String->Void = cpp.Lib.load("modioGlue","modioGlueInit",3);
+    static var modioGlueProcess:Void->Void = cpp.Lib.load("modioGlue","modioGlueProcess",0);
+    static var modioGlueIsLoggedIn:Void->Bool = cpp.Lib.load("modioGlue","modioGlueIsLoggedIn",0);
+    static var modioGlueLogout:Void->Bool = cpp.Lib.load("modioGlue","modioGlueLogout",0);
+    static var modioGlueEmailRequest:String->(Int->Void)->Void = cpp.Lib.load("modioGlue","modioGlueEmailRequest",2);
+    static var modioGlueEmailExchange:String->(Int->Void)->Void = cpp.Lib.load("modioGlue","modioGlueEmailExchange",2);
+    static var finished = false;
+
+    static function onEmailExchange(resonse_code:Int)
+    {
+        if (resonse_code == 200)
+        {
+            trace("Code exchanged!");
+        }
+        else
+        {
+            trace("Error exchanging code");
+        }
+        finished = true;
+        return;
+    }
+
+    static function onEmailRequest(resonse_code:Int)
+    {
+        if (resonse_code == 200)
+        {
+            trace("Email request successful");
+            
+            trace("Please enter the 5 digit security code:");
+            var security_code = Sys.stdin().readLine();
+            modioGlueEmailExchange(security_code, onEmailExchange);
+        }
+        else
+        {
+            trace("Error sending code");
+            finished = true;
+        }
+    }
+
     static function main()
     {
-        init(1, 7, "e91c01b8882f4affeddd56c96111977b");
+        modioGlueInit(1, 7, "e91c01b8882f4affeddd56c96111977b");
+
+        var is_logged_in = modioGlueIsLoggedIn();
+        if(!is_logged_in)
+        {
+            modioGlueEmailRequest("ahmed.hn.43@gmail.com", onEmailRequest);
+        }else
+        {
+            trace("You are already logged in. Do you want to logout? (y/n)");
+            var option = Sys.stdin().readLine();
+            if(option=="y")
+            {
+                modioGlueLogout();
+            }
+            finished = true;
+        }
+
+        while (!finished)
+        {
+            modioGlueProcess();
+        }
     }
 }
--- src/modioGlue.cpp
+++ src/modioGlue.cpp
@@ -5,26 +5,97 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <map>
 #include <modio/modio.h>
 
+u32 current_function = 0;
+std::map<int, value*> functions_stored;
 
-extern "C" {
-    value init(value modio_environment, value game_id, value api_key)
+void onEmailRequest(void *object, ModioResponse response)
+{
+    int function_number = *((int*)object);
+    value ret = val_call1(*(functions_stored[function_number]), alloc_int(response.code));
+}
+
+void onExchange(void *object, ModioResponse response)
+{
+    int function_number = *((int*)object);        
+    value ret = val_call1(*(functions_stored[function_number]), alloc_int(response.code));
+}
+
+extern "C"
+{
+    value modioGlueInit(value modio_environment, value game_id, value api_key)
     {
         u32 modio_environment_cpp;
         u32 game_id_cpp;
         std::string api_key_cpp;
 
-        if(val_is_int(modio_environment))
+        if (val_is_int(modio_environment))
             modio_environment_cpp = val_int(modio_environment);
-        if(val_is_int(game_id))
+        if (val_is_int(game_id))
             game_id_cpp = val_int(game_id);
-        if(val_is_string(api_key))
+        if (val_is_string(api_key))
             api_key_cpp = val_string(api_key);
-        
+
         modioInit(modio_environment_cpp, game_id_cpp, (char *)api_key_cpp.c_str());
 
-        return modio_environment;
+        return 0;
+    }
+
+    value modioGlueProcess()
+    {
+        modioProcess();
+    }
+
+    value modioGlueIsLoggedIn()
+    {
+        return alloc_bool(modioIsLoggedIn());
+    }
+
+    value modioGlueLogout()
+    {
+        modioLogout();
+    }
+
+    value modioGlueEmailRequest(value email, value callback)
+    {
+        val_check_function(callback, 1);
+        value *new_function = alloc_root();
+        *new_function = callback;
+        functions_stored[current_function] = new_function;
+
+        std::string email_cpp;
+
+        if (val_is_string(email))
+            email_cpp = val_string(email);
+        
+        modioEmailRequest(new int(current_function), (char *)email_cpp.c_str(), &onEmailRequest);
+
+        current_function++;
+    }
+
+    value modioGlueEmailExchange(value security_code, value callback)
+    {
+        val_check_function(callback, 1);
+        value *new_function = alloc_root();
+        *new_function = callback;
+        functions_stored[current_function] = new_function;
+
+        std::string security_code_cpp;
+
+        if (val_is_string(security_code))
+            security_code_cpp = val_string(security_code);
+
+        modioEmailExchange(NULL, (char *)security_code_cpp.c_str(), &onExchange);
+
+        current_function++;        
     }
 }
-DEFINE_PRIM( init, 3 );
+
+DEFINE_PRIM(modioGlueInit, 3);
+DEFINE_PRIM(modioGlueProcess, 0);
+DEFINE_PRIM(modioGlueEmailRequest, 2);
+DEFINE_PRIM(modioGlueEmailExchange, 2);
+DEFINE_PRIM(modioGlueIsLoggedIn, 0);
+DEFINE_PRIM(modioGlueLogout, 0);
