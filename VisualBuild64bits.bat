2356b1157f428c3b90ce5ef5cc1ebcd156cab035
turupawn <ahmed.hn.43@gmail.com>
2018-06-13 01:19:25 -0600

    modio.dll updated

--- VisualBuild64bits.bat
+++ VisualBuild64bits.bat
@@ -2,6 +2,6 @@
 mkdir bin
 cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp lib/windows64/modio.lib /I include /link /DLL /OUT:bin/modioWrapper.ndll
 haxe -cp src -main Main -cpp bin -D HXCPP_M64
-copy lib/windows64/modio.dll bin
+copy lib\windows64\modio.dll bin
 cd bin
 Main.exe
