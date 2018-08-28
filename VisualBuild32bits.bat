67a16b85bf523c3277afe183fe5941479f32033d
turupawn <ahmed.hn.43@gmail.com>
2018-08-28 00:11:42 -0600

    windows build updated to SDK v0.8.0

--- VisualBuild32bits.bat
+++ VisualBuild32bits.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir bin
-copy "mod.io-sdk-v0.7.0\bin\visualc++\x86\modio.dll" bin
-cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp ./src/modioWrapperCallbacks.cpp ./src/modioWrapperObjects.cpp lib/windows32/modio.lib /I include /I mod.io-sdk-v0.7.0/include /link /DLL /OUT:bin/modioWrapperWindows_x86.ndll
+copy "mod.io-sdk-v0.8.0\bin\nmake\x86\modio.dll" bin
+cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp ./src/modioWrapperCallbacks.cpp ./src/modioWrapperObjects.cpp mod.io-sdk-v0.8.0/lib/nmake/x86/modio.lib /I include /I mod.io-sdk-v0.8.0/include /link /DLL /OUT:bin/modioWrapperWindows_x86.ndll
 haxe -cp src -main Main -cpp bin -D windows
 cd bin
 Main.exe
