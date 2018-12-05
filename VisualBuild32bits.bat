cc1d18ec8a2881b303b93fbbd65521559ba5177a
turupawn <ahmed.hn.43@gmail.com>
2018-12-05 17:48:56 -0600

    updated windows to latest version

--- VisualBuild32bits.bat
+++ VisualBuild32bits.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir bin
-copy "mod.io-sdk-v0.10.0\bin\visualc++\x86\modio.dll" bin
-cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp ./src/modioWrapperCallbacks.cpp ./src/modioWrapperObjects.cpp mod.io-sdk-v0.10.0/lib/visualc++/x86/modio.lib /I include /I additional_dependencies /I mod.io-sdk-v0.10.0/include /link /DLL /OUT:bin/modioWrapperWindows_x86.ndll
+copy "mod.io-sdk-v0.10.1\bin\visualc++\x86\modio.dll" bin
+cl /D_USRDLL /D_WINDLL ./src/modioWrapper.cpp ./src/modioWrapperCallbacks.cpp ./src/modioWrapperObjects.cpp mod.io-sdk-v0.10.1/lib/visualc++/x86/modio.lib /I include /I additional_dependencies /I mod.io-sdk-v0.10.1/include /link /DLL /OUT:bin/modioWrapperWindows_x86.ndll
 haxe -cp src -main Main -cpp bin -D windows
 cd bin
 Main.exe
